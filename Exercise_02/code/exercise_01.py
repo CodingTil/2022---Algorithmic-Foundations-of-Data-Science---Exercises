094dcbc6c0017e18707499bea83c3452e9f0d59d
Til Valentin Mohr <til.mohr@rwth-aachen.de>
2022-04-19 22:52:17 +0200

    Exercise 02.01 done

--- Exercise_02/code/exercise_01.py
+++ Exercise_02/code/exercise_01.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot
+from sklearn.svm import LinearSVC
 
 S = [
     ((2, 1), -1),
@@ -10,7 +11,7 @@ S = [
 ]
 
 
-def plot(S, w):
+def plot(S, w_perc, w_svm):
     # scatter points
     x_values = [s[0][0] for s in S]
     y_values = [s[0][1] for s in S]
@@ -18,26 +19,29 @@ def plot(S, w):
 
     pyplot.scatter(x_values, y_values, c=colors)
 
-    # plot linear seperator
+    # plot linear seperators
     x_min = min(x_values)
     x_max = max(x_values)
     y_min = min(y_values)
     y_max = max(y_values)
 
-    ortho_w = (-w[1], w[0])
+    for w in [w_perc, w_svm]:
+        ortho_w = (-w[1], w[0])
 
-    p_1 = (x_min, ortho_w[1] * (x_min / ortho_w[0]))
-    p_2 = (x_max, ortho_w[1] * (x_max / ortho_w[0]))
-    p_3 = (ortho_w[0] * (y_min / ortho_w[1]), y_min)
-    p_4 = (ortho_w[0] * (y_max / ortho_w[1]), y_max)
+        p_1 = (x_min, ortho_w[1] * (x_min / ortho_w[0]))
+        p_2 = (x_max, ortho_w[1] * (x_max / ortho_w[0]))
+        p_3 = (ortho_w[0] * (y_min / ortho_w[1]), y_min)
+        p_4 = (ortho_w[0] * (y_max / ortho_w[1]), y_max)
 
-    p_x_values = (p_1[0], p_2[0], p_3[0], p_4[0])
-    p_y_values = (p_1[1], p_2[1], p_3[1], p_4[1])
+        p_x_values = (p_1[0], p_2[0], p_3[0], p_4[0])
+        p_y_values = (p_1[1], p_2[1], p_3[1], p_4[1])
 
-    pyplot.plot(p_x_values, p_y_values)
+        pyplot.plot(p_x_values, p_y_values, label=('Perceptron' if w==w_perc else 'SVM'))#
+
+    pyplot.legend()
 
     # save to file
-    pyplot.savefig('perceptron.png')
+    pyplot.savefig(f'exercise_01.png')
 
 
 def sgn(value) -> int:
@@ -77,6 +81,27 @@ def perceptron(S) -> tuple:
     return w
 
 
+def margin(S, w) -> float:
+    distances = [abs(dot_product(w, s[0]))/(dot_product(w, w)) for s in S]
+    distances = sorted(distances)
+    return distances[0]
+
+
+def svm(S) -> tuple:
+    classifier = LinearSVC(fit_intercept=False) # force heterogenous (fit_intercept=False)
+    classifier.fit([[s[0][0], s[0][1]] for s in S], [s[1] for s in S])
+    print(classifier.intercept_)
+    return (classifier.coef_[0][0], classifier.coef_[0][1])
+
+
 if __name__ == '__main__':
-    w = perceptron(S)
-    plot(S, w)
+    print(f'Perceptron Learning: \\\\ \n\\bigskip \n')
+    w_perc = perceptron(S)
+    print(f'Margin: ${margin(S,w_perc)}$')
+
+    print(f'SVM Learning: \\\\ \n\\bigskip \n')
+    w_svm = svm(S)
+    print(f'$w^*: {w_svm}$ \\\\')
+    print(f'Margin: ${margin(S, w_svm)}$')
+
+    plot(S, w_perc, w_svm)
