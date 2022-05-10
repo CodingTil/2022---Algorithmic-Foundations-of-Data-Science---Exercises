64a6daec8f6723e9f2f68296a05d72ec011743f2
Fabian Grob <redacted@example.com>
2022-05-10 19:46:00 +0200

    adds exercise 4a)

---
+++ Exercise_03/code/exercise_04.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+def mwu_algorithm(loss_matrix, events, rounds, alpha):
+    # initial weight vector of 1s
+    weights = np.ones((loss_matrix.shape[0]))
+    weights_tracking = {}
+    # more convenient to loop through rounds and events
+    rounds_arr = [i for i in range(rounds)]
+    for round, event in zip(rounds_arr, events):
+        # need to use event-1 as events start at 1 but indexing at 0
+        weights = np.power((1 - alpha), loss_matrix[:, event-1]) * weights
+        # getting the current probabilities, not really needed here
+        p = probabilities(weights)
+        # loss isn't really needed
+        loss = calculate_loss(loss_matrix, p, event-1)
+        weights_tracking[round] = weights
+    
+    return p, weights_tracking
+
+def probabilities(weights):
+    return weights / np.sum(weights)
+
+def calculate_loss(loss_matrix, probabilities, event):
+    return np.sum(probabilities * loss_matrix[:, event])
+
+
+loss_matrix = np.array([[0,1,1,0],
+                        [1,0,1,1],
+                        [1,1,0,0.5]])
+
+observed_events = [3,1,2,1,2,4]
+
+p_6, weights_tracking = mwu_algorithm(loss_matrix, observed_events, 6, alpha=0.5)
+
+print(f'Final probabilities: {p_6}\n')
+print(f'Tracked weight vectors (without initial one vector): \n')
+for key, val in weights_tracking.items():
+    print(f'Round:\t{key + 1}\tWeights:\t{val}')
